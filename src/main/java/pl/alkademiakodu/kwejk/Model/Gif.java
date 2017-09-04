5374d5e00daaac9b0497ebaedfb743e81234403b
John Bean <john@bean.com>
2017-09-04 16:09:18 +0200

    Gif class created

--- src/main/java/pl/alkademiakodu/kwejk/Model/Gif.java
+++ src/main/java/pl/alkademiakodu/kwejk/Model/Gif.java
@@ -1,7 +1,30 @@
-package pl.alkademiakodu.kwejk.Controller;
+package pl.alkademiakodu.kwejk.Model;
 
 /**
  * Created by User on 2017-09-04.
  */
 public class Gif {
+    private String name;
+    private String username;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Gif(String name, String username) {
+        this.name = name;
+        this.username = username;
+    }
 }
