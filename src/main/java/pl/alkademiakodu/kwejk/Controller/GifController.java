50d7c377c21b67110e684cf665dab666d20443d0
Marcin Wrobel <wrobel.sparrow@gmail.com>
2017-09-04 16:46:58 +0200

    commit before merge

--- src/main/java/pl/alkademiakodu/kwejk/Controller/GifController.java
+++ src/main/java/pl/alkademiakodu/kwejk/Controller/GifController.java
@@ -1,7 +1,11 @@
 package pl.alkademiakodu.kwejk.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pl.alkademiakodu.kwejk.dao.GifDao;
 
 /**
  * Created by User on 2017-09-04.
@@ -9,8 +13,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GifController {
 
+    @Autowired
+    private GifDao gifDao;
+
     @GetMapping("/")
     public String home(){
         return "home";
+    }
+
+    @GetMapping("/gif/{name}")
+    public String homeResult (@PathVariable String name, ModelMap modelMap) {
+        modelMap.addAttribute("gif",gifDao.findByName(name));
+        return "gif-details";
     }
 }
--- src/main/java/pl/alkademiakodu/kwejk/dao/GifDaoImp.java
+++ src/main/java/pl/alkademiakodu/kwejk/dao/GifDaoImp.java
@@ -1,10 +1,13 @@
 package pl.alkademiakodu.kwejk.dao;
 
+import org.springframework.stereotype.Component;
 import pl.alkademiakodu.kwejk.Model.Gif;
 
 import java.util.List;
 
+@Component
 public class GifDaoImp implements GifDao {
+
     @Override
     public List<Gif> findByName(String name) {
         return null;
