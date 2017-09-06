d887ad568fbadb2a29edb66edbe7c0bbd3bd5919
Marcin Wrobel <wrobel.sparrow@gmail.com>
2017-09-06 11:58:59 +0200

    menu view modified

--- src/main/java/pl/alkademiakodu/kwejk/Controller/GifController.java
+++ src/main/java/pl/alkademiakodu/kwejk/Controller/GifController.java
@@ -44,9 +44,8 @@ public class GifController {
     }
 
     @GetMapping("/categories")
-    public String categories(@RequestParam String q, ModelMap modelMap){
+    public String categories(ModelMap modelMap){
             modelMap.addAttribute("categories",gifDao.addCategories());
-
             return "categories";
     }
 
